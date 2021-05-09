4ff78f544211a68e997fc3120267a8b7c613f393
Kod2nd <kod2nd@gmail.com>
2021-05-09 21:34:42 +0800

    add analytics

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import './plugins/base'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: process.env.VUE_APP_GOOGLE_ANALYTICS,
+  router
+})
 
 new Vue({
   vuetify,
