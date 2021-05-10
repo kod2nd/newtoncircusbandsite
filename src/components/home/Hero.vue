4cd15a17f7b07771bd159a37c26f5fe2776833bc
Kod2nd <kod2nd@gmail.com>
2021-05-10 09:39:45 +0800

    resize hero based on screensize

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <v-card elevation="4">
-    <v-img
-      :src="require('@/assets/hero2.jpg')"
-      position="top"
-      height="100vh"
-      width="100%"
-      :elevation="10"
-      class="nc-hero"
-    >
-    </v-img>
+      <v-img
+        :src="require('@/assets/hero2.jpg')"
+        position="top"
+        :height="height"
+        width="100%"
+        :elevation="10"
+        class="nc-hero"
+      >
+      </v-img>
     </v-card>
   </div>
 </template>
@@ -24,6 +24,19 @@
 export default {
   name: "Hero",
 
+  data: () => ({ isMobile: false,
+  height: "100vh" }),
+  beforeDestroy() {
+    if (typeof window === "undefined") return;
+
+    window.removeEventListener("resize", this.onResize, { passive: true });
+  },
+  mounted() {
+    this.onResize();
+    console.log(this.$vuetify.breakpoint.width)
+
+    window.addEventListener("resize", this.onResize, { passive: true });
+  },
   props: {
     square: Boolean,
     text: String,
@@ -32,6 +45,14 @@ export default {
     onClick(e) {
       e.stopPropagation();
       this.$vuetify.goTo("#about");
+    },
+    onResize() {
+      this.isMobile = window.innerWidth < 600;
+      if (this.isMobile) {
+        this.height = "50vh"
+      } else {
+        this.height = "100vh"
+      }
     },
   },
 
